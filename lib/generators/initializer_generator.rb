fa2cea9ed4de57f1714ff06bc1b760368796e56b
gravindra <ravighare26@gmail.com>
2013-03-30 16:45:12 +0530

    First commit

---
+++ lib/generators/initializer_generator.rb
@@ -0,0 +1,31 @@
+class InitializerGenerator < Rails::Generators::Base
+  source_root File.expand_path('../templates', __FILE__)  
+
+  def create_initializer_file
+    str = %!#This file loads simileys
+module SmileyHelpers
+  def replace_smilies(str)
+    smilies = YAML.load_file("\#{Rails.root}/config/smilies.yml")
+    smilies.each_pair do |k, v|
+      new_str = "<span class=\\'smilies "+k+"\\'>&nbsp;</span>"
+      str = str.gsub(v, new_str)
+    end
+    str.html_safe
+  end
+end
+ActionView::Base.send :include, SmileyHelpers
+    !
+
+    copy_file "smilies.yml", "#{Rails.root}/config/smilies.yml"
+
+    if Dir.exists?("#{Rails.root}/app/assets/")
+      copy_file "smilies-sprite.png", "#{Rails.root}/app/assets/images/smilies-sprite.png"
+      copy_file "smilies.css", "#{Rails.root}/app/assets/stylesheets/smilies.css"
+    else
+      copy_file "smilies-sprite.png", "#{Rails.root}/public/images/smilies-sprite.png"
+      copy_file "smilies.css", "#{Rails.root}/public/stylesheets/smilies.css"
+    end
+
+    create_file "config/initializers/smileys.rb", str
+  end
+end
